c0d8d20ff169e959ccee28c8ca7dcaca0399124e
Eric Berg <eric.berg@salesforce.com>
2013-12-15 22:21:48 -0500

    more pounders

--- demo/queueTest.sh
+++ demo/queueTest.sh
@@ -1,9 +1,29 @@
 #!/bin/sh
 
-# /usr/bin/php publisherPounder.php &
+/usr/bin/php publisherPounder.php &
 
-# PID=$!
+PID0=$!
+
+/usr/bin/php publisherPounder.php &
+
+PID1=$!
+
+/usr/bin/php publisherPounder.php &
+
+PID2=$!
+
+/usr/bin/php publisherPounder.php &
+
+PID3=$!
+
+/usr/bin/php publisherPounder.php &
+
+PID4=$!
 
 /usr/bin/php publisherSpecificExchange.php 50000 5 1
 
-# sudo kill -9 $PID
+sudo kill -9 $PID
+sudo kill -9 $PID1
+sudo kill -9 $PID2
+sudo kill -9 $PID3
+sudo kill -9 $PID4
