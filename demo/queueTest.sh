83ef96774cb63805a5a56e215300450559df7dff
Eric Berg <eric.berg@salesforce.com>
2013-12-17 13:47:38 -0500

    add args

--- demo/queueTest.sh
+++ demo/queueTest.sh
@@ -1,44 +1,21 @@
 #!/bin/sh
 
-/usr/bin/php publisherPounder.php &
-PID0=$!
+LIMIT=$1
+COUNTER=0
+echo Launching $LIMIT pounders
 
-/usr/bin/php publisherPounder.php &
-PID1=$!
+PIDS=()
+while [  $COUNTER -lt $LIMIT ]; do
+    /usr/bin/php publisherPounder.php &
+    PIDS+=("$!")
+    let COUNTER+=1
+done
 
-/usr/bin/php publisherPounder.php &
-PID2=$!
-
-/usr/bin/php publisherPounder.php &
-PID3=$!
-
-/usr/bin/php publisherPounder.php &
-PID4=$!
-
-/usr/bin/php publisherPounder.php &
-PID5=$!
-
-/usr/bin/php publisherPounder.php &
-PID6=$!
-
-/usr/bin/php publisherPounder.php &
-PID7=$!
-
-/usr/bin/php publisherPounder.php &
-PID8=$!
-
-/usr/bin/php publisherPounder.php &
-PID9=$!
 
 /usr/bin/php publisherSpecificExchange.php 50000 5 1
 
-sudo kill -9 $PID0
-sudo kill -9 $PID1
-sudo kill -9 $PID2
-sudo kill -9 $PID3
-sudo kill -9 $PID4
-sudo kill -9 $PID5
-sudo kill -9 $PID6
-sudo kill -9 $PID7
-sudo kill -9 $PID8
-sudo kill -9 $PID9
+for P in $PIDS
+do
+    echo Killing $P
+    sudo kill $P
+done
